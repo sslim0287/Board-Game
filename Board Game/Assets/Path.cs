114fe4351cc7eb90627eabcf4cafaacbd9fd1d04
Lim Sing Seng <sslim0287@gmail.com>
2021-09-27 15:41:05 +0800

    PlayerMovement&DiceRoll

---
+++ Board Game/Assets/Path.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class Path : MonoBehaviour {
+
+    public Transform[] points;
+
+    [SerializeField]
+    private float movement = 1f;
+
+    [HideInInspector]
+    public int pointsIndex = 0;
+
+    public bool moveAllowed = false;
+
+	// Use this for initialization
+	private void Start () {
+        transform.position = points[pointsIndex].transform.position;
+	}
+	
+	// Update is called once per frame
+	private void Update () {
+        if (moveAllowed)
+            Move();
+	}
+
+    private void Move()
+    {
+        if (pointsIndex <= points.Length - 1)
+        {
+            transform.position = Vector2.MoveTowards(transform.position,
+            points[pointsIndex].transform.position,
+            movement * Time.deltaTime);
+
+            if (transform.position == points[pointsIndex].transform.position)
+            {
+                pointsIndex += 1;
+            }
+        }
+    }
+}
